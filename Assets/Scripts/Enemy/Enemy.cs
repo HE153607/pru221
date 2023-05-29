85f72860005ceae8a1b20be93efa080330330d8d
quangthai <quangthai05122001@gmail.com>
2023-05-29 21:00:06 +0700

    update enemy rotate

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,38 +6,38 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public static Action<Enemy> OnEndReached;
-    
+
     [SerializeField] private float moveSpeed = 3f;
 
     /// <summary>
     /// Move speed of our enemy
     /// </summary>
     public float MoveSpeed { get; set; }
-    
+
     /// <summary>
     /// The waypoint reference
     /// </summary>
     public Waypoint Waypoint { get; set; }
 
     public EnemyHealth EnemyHealth { get; set; }
-    
+
     /// <summary>
     /// Returns the current Point Position where this enemy needs to go
     /// </summary>
     public Vector3 CurrentPointPosition => Waypoint.GetWaypointPosition(_currentWaypointIndex);
-    
+
     private int _currentWaypointIndex;
     private Vector3 _lastPointPosition;
-    
+
     private EnemyHealth _enemyHealth;
     private SpriteRenderer _spriteRenderer;
-    
+
     private void Start()
     {
         _enemyHealth = GetComponent<EnemyHealth>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         EnemyHealth = GetComponent<EnemyHealth>();
-        
+
         _currentWaypointIndex = 0;
         MoveSpeed = moveSpeed;
         _lastPointPosition = transform.position;
@@ -47,16 +47,16 @@ public class Enemy : MonoBehaviour
     {
         Move();
         Rotate();
-        
+
         if (CurrentPointPositionReached())
         {
             UpdateCurrentPointIndex();
         }
     }
-    
+
     private void Move()
     {
-        transform.position = Vector3.MoveTowards(transform.position, 
+        transform.position = Vector3.MoveTowards(transform.position,
             CurrentPointPosition, MoveSpeed * Time.deltaTime);
     }
 
@@ -74,14 +74,16 @@ public class Enemy : MonoBehaviour
     {
         if (CurrentPointPosition.x > _lastPointPosition.x)
         {
-            _spriteRenderer.flipX = false;
+            //_spriteRenderer.flipX = false;
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
         }
         else
         {
-            _spriteRenderer.flipX = true;
+            //_spriteRenderer.flipX = true;
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
         }
     }
-    
+
     private bool CurrentPointPositionReached()
     {
         float distanceToNextPointPosition = (transform.position - CurrentPointPosition).magnitude;
